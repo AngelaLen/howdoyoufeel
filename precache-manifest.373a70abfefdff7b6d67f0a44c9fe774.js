cc775383766238f429d8a50c613a03aaa2ec4754
Angela Lengkeek <angelalengkeek@gmail.com>
2019-07-11 12:56:40 +0200

    Updates

---
+++ precache-manifest.373a70abfefdff7b6d67f0a44c9fe774.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4ddaf1a2f2d299b2a282ded2146ea4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d76729212a69429038",
+    "url": "/static/css/main.76a4b511.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "72d76729212a69429038",
+    "url": "/static/js/main.7f28adcc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
